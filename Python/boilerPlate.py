8f3d8cca457a4088d6725a1a68b56ac35a3114bf
Hanna Nabil <hananabil@live.com>
2019-01-17 00:44:31 +0200

    added list lab assignment

---
+++ Python/boilerPlate.py
@@ -0,0 +1,9 @@
+import sys
+def it():
+	print('from ITI')
+print ('lol')
+if __name__ == '__main__':
+	print("iti -->> __name__ = '__main____'")
+	
+if __name__ == '__iti__' :
+	print("iti -->> __name__ = '__ITI____'")
